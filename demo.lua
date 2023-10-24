43e21ac14c045cdf61c87f98b8f1b1ba34ccfea6
Maxkowa <93106024+Maxkowa@users.noreply.github.com>
2023-10-24 07:02:58 -0700

    Add files via upload

--- demo.lua
+++ demo.lua
@@ -217,7 +217,7 @@ end)
 demo:Init(function() 
     if player.mounted then return end 
     if player.casting or player.channeling then return end
-        if not player.casting and not player.channeling then
+    if not player.casting and not player.channeling then
         felguard()
         lifeTap()
         summonFelguard()
@@ -232,7 +232,6 @@ demo:Init(function()
         incinerate()
         shadowflame()
         seedOfCorruption()
-        end
     end
 end)
 

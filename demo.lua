08ebe3f480cb31ab14a685e67c89c3a4e0877cca
Maxkowa <93106024+Maxkowa@users.noreply.github.com>
2023-10-24 06:44:51 -0700

    Add files via upload

--- demo.lua
+++ demo.lua
@@ -66,7 +66,7 @@ awful.Populate({
     haunt = spell({48181, 59164, 59165, 59166}, { damage = "magic", targeted = true }),
     howlOfTerror = spell({5484, 17928}, { effect = "magic", targeted = true }),
     lifeTap = spell({1454, 1455, 1456, 11687, 11688, 11689, 27222, 47855, 47856}, { beneficial = true }),
-    seedOfCorruption = spell({27243, 47843, 47844}, { damage = "magic", targeted = true }),
+    seedOfCorruption = spell({27243, 47843, 47844}, { damage = "magic", targeted = true, radius = 8 }),
     unstableAffliction = spell({30108, 30404, 30405, 47843, 47844}, { damage = "magic", targeted = true }),
     drainSoul = spell({1120, 8288, 8289, 11675, 11676, 27217}, { damage = "magic", targeted = true }),
     chaosBolt = spell({50796, 59172, 59173, 59174, 59175}, { damage = "magic", targeted = true }),

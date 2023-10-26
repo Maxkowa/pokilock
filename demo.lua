c18ca7c33e7fd1f9b4c2b9074a4ba38dc498059c
Maxkowa <93106024+Maxkowa@users.noreply.github.com>
2023-10-26 03:16:02 -0700

    Add files via upload

--- demo.lua
+++ demo.lua
@@ -215,7 +215,7 @@ function WasCastingCheck()
         wasCasting[player.castingid] = time
     end
     for spell, when in pairs(wasCasting) do
-        if time - when > 0.300 + awful.buffer then
+        if time - when > 0.500 + awful.buffer then
             wasCasting[spell] = nil
         end
     end
